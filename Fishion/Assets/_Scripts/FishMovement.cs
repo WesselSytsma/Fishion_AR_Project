7e4c5414d40ab6f45967df452aa5c6dcec68bd78
WesselSytsma <wswes@hotmail.com>
2015-10-08 16:47:13 +0200

    Added Detection ray for the fish

---
+++ Fishion/Assets/_Scripts/FishMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMovement : MonoBehaviour
+{
+    public float DetectionRange;
+
+	void Update ()
+    {
+        int layerMask = 1 << 8;
+        RaycastHit Hit;
+
+        if(Physics.Raycast(transform.position, Vector3.left, out Hit, DetectionRange, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * DetectionRange, Color.blue);
+            Debug.Log(Hit.collider.tag);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * DetectionRange, Color.yellow);
+        }
+	}
+}
