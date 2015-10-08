e1e4acb760adc62cf2f6d48ff59ac4913ddb09d0
WesselSytsma <wswes@hotmail.com>
2015-10-08 12:23:46 +0200

    Fish tank added to the StereoAR scene

---
+++ Fishion/Assets/Scripts/SplashAndLoad/ErrorManager.cs
@@ -0,0 +1,20 @@
+﻿/*============================================================================== 
+ * Copyright (c) 2015 Qualcomm Connected Experiences, Inc. All Rights Reserved. 
+ * ==============================================================================*/
+using UnityEngine;
+using System.Collections;
+
+public class ErrorManager : MonoBehaviour
+{
+    #region PUBLIC_METHODS
+    public void OnErrorDialogDismiss()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // Exit app
+        Application.Quit();
+#endif
+    }
+    #endregion // PUBLIC_METHODS
+}
