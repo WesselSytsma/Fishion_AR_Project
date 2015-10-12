ad481eb8362ae58295f8837c9b5d8def18e81a18
flyzza <zfy1995@hotmail.com>
2015-10-12 17:23:07 +0200

    Lose health
    lose health after touching enemy fish script

---
+++ Fishion/Assets/_Scripts/FishEnemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FishEnemy : MonoBehaviour {
+
+    public Text Health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Score.health --;
+        }
+    }
+}
--- Fishion/Assets/_Scripts/Score.cs
+++ Fishion/Assets/_Scripts/Score.cs
@@ -6,18 +6,21 @@ public class Score : MonoBehaviour {
 
     public static int score;
     public Text scoreText;
+    public Text healthText;
+    public static int health;
 
     // Use this for initialization
     void Start () {
-
-        scoreText = GetComponent<Text>();
+        
         score = 0;
+        health = 3;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         scoreText.text = "Fished: " + score;
+        healthText.text = "Health: " + health;
 
     }
 }
