d721dbfb3771884eb188a2afffc633bcedafb891
WesselSytsma <wswes@hotmail.com>
2015-10-13 15:08:40 +0200

    Added rigidbody to the Fish prefab
    and still working on the fish movement in the mean time.

--- Fishion/Assets/_Scripts/FishMovement.cs
+++ Fishion/Assets/_Scripts/FishMovement.cs
@@ -3,21 +3,45 @@ using System.Collections;
 
 public class FishMovement : MonoBehaviour
 {
+    public Rigidbody rb;
     public float DetectionRange;
+    public float Speed;
+
+    Vector3 Direction;
+    int layerMask;
+    RaycastHit Hit;
+
+    void Start()
+    {
+        Direction = Random.onUnitSphere;
+        layerMask = 1 << 8;
+    }
 
 	void Update ()
     {
-        int layerMask = 1 << 8;
-        RaycastHit Hit;
+        //transform.Translate((Direction * Speed) * Time.deltaTime);
+
+        if (rb.velocity != new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
 
         if(Physics.Raycast(transform.position, Vector3.left, out Hit, DetectionRange, layerMask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * DetectionRange, Color.blue);
-            Debug.Log(Hit.collider.tag);
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * DetectionRange, Color.yellow);
         }
+
+        OnImpact();
 	}
+
+    void OnImpact()
+    {
+        
+        Debug.DrawRay(transform.position, transform.TransformDirection(new Vector3(-0.75f, 0.0f, 0.0f)) * DetectionRange, Color.green);
+
+    }
 }
---
+++ Fishion/Assets/_Scripts/SpawnerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerBehaviour : MonoBehaviour {
+    public GameObject SpawnPoint;
+    public GameObject FishPreFab;
+
+    int NumberOfFish = 0;
+    Vector3 FishSpawnPoint;
+
+    void Start () {
+        FishSpawnPoint = SpawnPoint.transform.position;
+
+        for (int i = 0; i < 10; i++)
+        {
+            NumberOfFish++;
+            SpawnNewFish();
+        }
+	}
+	
+	void Update () {
+        if (NumberOfFish < 10)
+        {
+            SpawnNewFish();
+        }
+	    
+	}
+
+    public void SpawnNewFish()
+    {
+        GameObject Fish = (GameObject)Instantiate(FishPreFab, FishSpawnPoint, Quaternion.identity);
+    }
+}
