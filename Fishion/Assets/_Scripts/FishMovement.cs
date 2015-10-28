ca01832521d16e68f9a6a3ced324e12885f9d409
WesselSytsma <wswes@hotmail.com>
2015-10-28 17:26:55 +0100

    Tinkered with the Movement script
    found the problem lies with the LookRotation

--- Fishion/Assets/_Scripts/FishMovement.cs
+++ Fishion/Assets/_Scripts/FishMovement.cs
@@ -8,6 +8,7 @@ public class FishMovement : MonoBehaviour
 
     int layerMask;
     Vector3 direction;
+    Vector3 targetPosition;
 
     Vector3 leftOfset;
     Vector3 rightOfset;
@@ -17,7 +18,7 @@ public class FishMovement : MonoBehaviour
         direction = Random.onUnitSphere;
         layerMask = 1 << 8;
 
-        Debug.Log(direction);
+        //Debug.Log(direction);
     }
 
 	void Update ()
@@ -44,12 +45,12 @@ public class FishMovement : MonoBehaviour
         if (Physics.Raycast(leftOfset, Vector3.forward, out Hit, detectionRange, layerMask))
         {	//needs to turn right
 			Quaternion myRotation = Quaternion.Euler(0.0f, 10.0f, 0.0f);
-			direction = myRotation * direction;
+            direction = (myRotation * direction).normalized;
         }
         else if (Physics.Raycast(rightOfset, Vector3.forward, out Hit, detectionRange, layerMask))
         {	//needs to turn left
 			Quaternion myRotation = Quaternion.Euler(0.0f, -10.0f, 0.0f);
-			direction = myRotation * direction;
+            direction = (myRotation * direction).normalized;
         }
 
         //showing the position of the rays in the scene window, 1st Left, 2nd Right
@@ -65,13 +66,13 @@ public class FishMovement : MonoBehaviour
         {	//needs to tilt downwards
             //int RandomDegreesDown = Random.Range(-40, -15);
 			Quaternion myRotation = Quaternion.Euler(-30.0f, 0.0f, 0.0f);
-			direction = myRotation * direction;
+            direction = (myRotation * direction).normalized;
         }
         else if (Physics.Raycast(transform.position, new Vector3(0.0f, -0.7f, 0.7f), out Hit, detectionRange, layerMask))
         {	//nees to tilt upwards
             //int RandomDegreesUp = Random.Range(15, 40);
 			Quaternion myRotation = Quaternion.Euler(30.0f, 0.0f, 0.0f);
-			direction = myRotation * direction;
+            direction = (myRotation * direction).normalized;
         }
 
         //showing the position of the rays in the scene window, 1st Top, 2nd Bottom
