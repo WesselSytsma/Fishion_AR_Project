e50d72ff303958fcd16666e341fcab1935e336ce
flyzza <zfy1995@hotmail.com>
2015-11-03 16:24:20 +0100

    Fishtargets done
    done and done
    just need name changed to have the correct names with the prefabs

--- Fishion/Assets/_Scripts/FishTargetDisplay.cs
+++ Fishion/Assets/_Scripts/FishTargetDisplay.cs
@@ -1,30 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using System.Collections;
 
 public class FishTargetDisplay : MonoBehaviour {
 
-    public Sprite[] fishTargets;
     private Sprite currentTarget;
     public Image fishDisplay;
+    private List<string> fishSprites = new List<string>();
+    public string currentTargetName;
+
+    public GameObject[] fishprefabs;
+
 
 
     // Use this for initialization
     void Start () {
-        RandomizeTargets();
+        PrefabsToString();
+        NextTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.Log(currentTargetName);
 	
 	}
 
-    void RandomizeTargets()
+    public void NextTarget()
     {
         int targetNumber;
-        string targetName;
-        targetNumber = Random.Range(0, (fishTargets.Length));
-        targetName = fishTargets[targetNumber].name;
-        fishDisplay.sprite = Resources.Load<Sprite>("Sprites/" + targetName);
+        targetNumber = Random.Range(0, (fishSprites.Count));
+        currentTargetName = fishSprites[targetNumber];
+        fishDisplay.sprite = Resources.Load<Sprite>("Sprites/" + currentTargetName);
+        fishSprites.RemoveAt(targetNumber);
+    }
+    
+    void PrefabsToString()
+    {
+        for (int i = 0; i < fishprefabs.Length; i++)
+        {
+            if (fishprefabs[i].name == "Fish")
+            {
+                fishSprites.Add("oval");
+            }
+            else if(fishprefabs[i].name == "OtherFish")
+            {
+                fishSprites.Add("rectancle");
+            }
+            else if(fishprefabs[i].name == "asd")
+            {
+                fishSprites.Add("round");
+            }
+            else if(fishprefabs[i].name == "anglefish")
+            {
+                fishSprites.Add("triangle");
+            }
+        }
     }
 }
--- Fishion/Assets/_Scripts/Fishnet.cs
+++ Fishion/Assets/_Scripts/Fishnet.cs
@@ -5,10 +5,15 @@ public class Fishnet : MonoBehaviour {
 
     public GameObject fishnet;
     public bool fishInNet = false;
+    private string fishedName;
+    private string currentFish;
+    public GameObject canvas;
+    //private FishTargetDisplay fishTargetDisplay;
+    public int scoreValue = 10;
+    private GameObject catchedFish;
 
     // Use this for initialization
     void Start () {
-
     }
 
     // Update is called once per frame
@@ -18,16 +23,53 @@ public class Fishnet : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other)
-    { 
-        if(other.tag == "Fish" && fishInNet == false)
+    {
+        if (other.tag == "Fish" && fishInNet == false)
         {
             other.transform.parent = fishnet.transform;
+            fishedName = other.name;
             fishInNet = true;
+            catchedFish = other.gameObject;
+        }
+        else if (other.name == "Releaser")
+        {
+            changeNames();
+            if (currentFish == canvas.GetComponent<FishTargetDisplay>().currentTargetName && fishInNet == true)
+            {
+                Score.score += scoreValue;
+                canvas.GetComponent<FishTargetDisplay>().NextTarget();
+            }
+            else
+            {
+                Score.health--;
+            }
+            Destroy(catchedFish);
+            fishInNet = false;
         }
     }
     
-    public void EmptyOutNet()
+    public string getFishName()
     {
-        fishInNet = false;
+        return fishedName;
+    }
+
+    void changeNames()
+    { 
+        if (fishedName == "Fish")
+        {
+            currentFish = "oval";
+        }
+        else if (fishedName == "sad")
+        {
+            currentFish = "rectancle";
+        }
+        else if (fishedName == "asd")
+        {
+            currentFish = "round";
+        }
+        else if (fishedName == "anglefish")
+        {
+            currentFish = "triangle";
+        }
     }
 }
